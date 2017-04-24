a9795c7f12cdc4b7f582879f6098167fc50315f1
Hoang Nguyen <hoanghnguyen@kms-technology.com>
2017-04-24 23:00:00 +0700

    include materialize library

--- index.js
+++ index.js
@@ -2,5 +2,11 @@
 'use strict';
 
 module.exports = {
-  name: 'ember-cli-materialize-css'
+  name: 'ember-cli-materialize-css',
+  included: function (app) {
+    this._super.included.apply(this, arguments);
+
+    app.import(app.bowerDirectory + '/materialize/dist/js/materialize.js');
+    app.import(app.bowerDirectory + '/materialize/dist/css/materialize.css');
+  }
 };
