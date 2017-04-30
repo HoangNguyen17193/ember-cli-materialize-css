9db86e4e31854fcc4250cc6befb114c3ac0e0695
Hoang Nguyen <hoanghnguyen@kms-technology.com>
2017-04-30 12:47:56 +0700

    add floating-button component

---
+++ addon/components/em-floating-button.js
@@ -0,0 +1,6 @@
+import EMButton from './em-button';
+import layout from '../templates/components/em-floating-button';
+
+export default EMButton.extend({
+  layout
+});
---
+++ app/components/em-floating-button.js
@@ -0,0 +1,3 @@
+import EMFloatingButton from 'ember-cli-materialize-css/components/em-floating-button';
+
+export default EMFloatingButton;
